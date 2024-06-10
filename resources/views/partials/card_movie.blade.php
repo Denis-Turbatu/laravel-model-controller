10784c5b0427af918dd942f7d210a9cbb3d38ee1
Denis-Turbatu <turbatudenis81@gmail.com>
2024-06-10 15:32:45 +0200

    feat: displayed all data from DB for each movie

--- resources/views/partials/card_movie.blade.php
+++ resources/views/partials/card_movie.blade.php
@@ -1,6 +1,20 @@
 <div class="card" style="width: 18rem;">
     <img src="{{ asset($movie['image']) }}" class="card-img-top" alt="{{ $movie['title'] }}">
     <div class="card-body">
-        <p class="card-text"></p>
+        <p class="card-text">
+            Titolo: {{ $movie['title'] }}
+        </p>
+        <p class="card-text">
+            Titolo originale: {{ $movie['original_title'] }}
+        </p>
+        <p class="card-text">
+            Nazionalità: {{ $movie['nationality'] }}
+        </p>
+        <p class="card-text">
+            Data d'uscita: {{ $movie['date'] }}
+        </p>
+        <p class="card-text">
+            Rating: {{ $movie['vote'] }}
+        </p>
     </div>
 </div>
